ffe2391e140594129a2b71af6a9fb097dcf169c1
Matt <aboutmattlaw@gmail.com>
2022-01-17 18:06:17 -0500

    text changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
    <>
-   <h1>This will be where we track movies for our Friday movie nights.</h1>
+   <h1>This will be where we track movies for our Friday movie nights!</h1>
    </>
   );
 }
